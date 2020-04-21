270ba0cf9f53d5e4cb840e2895fb25a99e369b2c
Raudin Moreno <raudin247@gmail.com>
2020-04-21 14:37:33 -0400

    First commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Raudin Moreno on 4/21/20.
+//  Copyright © 2020 Raudin Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
